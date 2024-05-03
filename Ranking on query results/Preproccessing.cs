2e13e22acb0e8ebd464cbac0ce62ed8667d70743
Julianvanbijlert <116498773+Julianvanbijlert@users.noreply.github.com>
2024-05-03 23:51:27 +0200

    idk

---
+++ Ranking on query results/Preproccessing.cs
@@ -0,0 +1,24 @@
+using System;
+
+//The first program will do some preprocessing on the data and/or the workload.
+//During this phase, a meta database will be constructed and filled.
+//This metadb will be used when answering the actual queries.
+
+
+
+public class Preprocessing
+{
+    public void processquery(string query)
+    {
+        
+    }
+
+    public void processconjunction(string conjunction)
+    {
+        
+    }
+    public static void Main()
+    {
+        Console.WriteLine("Preprocessing");
+    }
+}
---
+++ Ranking on query results/QueryProcessing.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+//The second program will be able to process queries and show the answers,
+//making use of the metadb for ranking.
+//Note that your metadb should be constructed only once,
+//for this particular contents of the db and before processing a batch of queries.
+
+
+
+/*
+ *(Her)definities
+   Gegeven attribuut Ak
+   Voor elke waarde v uit dom(Ak) is Fk(v) het aantal tuples in R met Ak = v
+   Laat N = |R| ;
+   we definiëren idfk v) = log(N/Fk(v))
+ *
+ */
